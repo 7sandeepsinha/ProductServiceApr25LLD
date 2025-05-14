750418f9a33855bca125685ff625eaf0841cf32e
sndpoffcl <sandeep2official@gmail.com>
2025-05-14 21:53:15 +0530

    Added scheduler and CRON tasks

--- src/main/java/dev/sandeep/ProductServiceApr25/ProductServiceApr25Application.java
+++ src/main/java/dev/sandeep/ProductServiceApr25/ProductServiceApr25Application.java
@@ -2,8 +2,10 @@ package dev.sandeep.ProductServiceApr25;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ProductServiceApr25Application {
 
 	public static void main(String[] args) {
